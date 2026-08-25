dd2ca9db166b3127e6d3aee2911704c6f848dd3a
Vasiliy Lezhnin <vasiliylezhnin@MacBook-Pro-Vasiliy.local>
2026-08-25 16:40:20 +0500

    countdown_up_1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,25 @@
+let intervalId;
+let timerLeft = parseInt(document.getElementById('timer').textContent);
+
 const lower = function () {
   const timer = document.getElementById('timer');
-  let timerLeft = parseInt(timer.textContent);
-  if (timerLeft === 0) {
+  function formatSeconds(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    return [hours, minutes, seconds]
+      .map(v => String(v).padStart(2, '0'))
+      .join(':');
+}
+
+    if (timerLeft === 0) {
     alert("Вы победили в конкурсе!");
+    clearInterval(intervalId);
   } else {
     timerLeft--;
-    timer.textContent = timerLeft;
+    timer.textContent = formatSeconds(timerLeft);
   }
 }
-setInterval(lower, 1000);
+lower();
+intervalId = setInterval(lower, 1000);
