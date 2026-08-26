f4d3b047620271899be84a3ce9e04a518a754ea9
Vasiliy Lezhnin <vasiliylezhnin@MacBook-Pro-Vasiliy.local>
2026-08-26 10:27:33 +0500

    cookie_up

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,22 +1,29 @@
-const countElement = document.getElementById('clicker__counter');
-const cookieImg = document.getElementById('cookie');
+const cookie = document.getElementById("cookie");
+const countUp = document.getElementById("clicker__counter");
+const clickSpeed = document.getElementById("clicker__speed");
 
+let lastClickTime = 0;
 let clicks = 0;
 
-let zoomImg = true;
-const sizeImg = 200;
-const changeSizeImg = 50;
+function clickCookie() {
 
-cookieImg.onclick = function () {
-    clicks++;
-    countElement.textContent = clicks;
-    if (zoomImg) {
-        cookieImg.width = sizeImg - changeSizeImg;
-        cookieImg.height = sizeImg - changeSizeImg;
+    clicker__counter.textContent++;
+
+    if (cookie.width === 250) {
+        cookie.width = 200;
     } else {
-
-        cookieImg.width = sizeImg + changeSizeImg;
-        cookieImg.height = sizeImg + changeSizeImg;
+    cookie.width = 250;
     }
-    zoomImg = !zoomImg;
+
+    const now = Date.now();
+    clicks++;
+    if (lastClickTime > 0) {
+        const timeClicked = now - lastClickTime;
+        const speed = timeClicked/1000;
+        clicker__speed.textContent = speed.toFixed(2);
+    }
+    lastClickTime = now;
+
 }
+        
+cookie.onclick = clickCookie;
